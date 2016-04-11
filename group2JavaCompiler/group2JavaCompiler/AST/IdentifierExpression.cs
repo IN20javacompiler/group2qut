414ad7997489b1b8d7af39585e4a430f5c636374
Pretty Kurian <pskpretty@gmail.com>
2016-04-11 13:25:28 +1000

    add all class files for AST

---
+++ group2JavaCompiler/group2JavaCompiler/AST/IdentifierExpression.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace group2JavaCompiler.AST
+{
+    class IdentifierExpression
+    {
+        private string name;
+        public IdentifierExpression(string name)
+        {
+            this.name = name;
+        }
+    }
+}
