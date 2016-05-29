d8a0d74518a0287a8556ea5c923d69b82aa195e2
Lissy Lawrence <llissuj@gmail.com>
2016-05-29 19:48:13 +1000

    Parser file modified for AST

---
+++ group2JavaCompiler/group2JavaCompiler/AST/Parameter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace group2JavaCompiler.AST
+{
+    public class Parameter : Node
+    {
+        Type type;
+        VariableDeclaratorId id;
+        string modifiers;
+        private bool isVarArgs;
+
+        public Parameter()
+        {
+        }
+
+        public Parameter(Type type, VariableDeclaratorId id)
+        {
+            this.id = id;
+            this.type = type;
+        }
+
+        public Parameter(string modifiers, Type type, VariableDeclaratorId id)
+        {
+            this.modifiers = modifiers;
+            this.id = id;
+            this.type = type;
+        }
+
+        public Parameter(string modifiers, Type type, bool isVarArgs, VariableDeclaratorId id)
+        {
+            this.isVarArgs = isVarArgs;
+            this.modifiers = modifiers;
+            this.id = id;
+            this.type = type;
+        }
+
+        public override void dump(int indent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
