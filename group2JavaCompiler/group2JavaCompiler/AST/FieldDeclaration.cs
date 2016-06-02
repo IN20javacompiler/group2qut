a9e0db3341e392f3d491fad89902cd8f717a4dff
Brar <Brar>
2016-06-02 17:29:03 +1000

    FieldDeclaration AST

--- group2JavaCompiler/group2JavaCompiler/AST/FieldDeclaration.cs
+++ group2JavaCompiler/group2JavaCompiler/AST/FieldDeclaration.cs
@@ -12,12 +12,12 @@ namespace group2JavaCompiler.AST
         string modifier, name;
         List<VariableDeclarationStatement> variableList;
         Type type;
-        /*public FieldDeclaration(string modifier, Type type, VariableDeclarationStatement variable)
+        public FieldDeclaration(string modifier, Type type, VariableDeclarationStatement variable)
         {
             this.modifier = modifier;
             this.type = type;
             this.varDeclarationstmt = variable;
-        }*/
+        }
         public FieldDeclaration(string modifier, Type type, List<VariableDeclarationStatement> variable)
         {
             this.modifier = modifier;
