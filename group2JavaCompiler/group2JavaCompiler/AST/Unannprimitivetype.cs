36a69545f71729edb2576ccd5ed57fedc6165442
Seema P <seemsniralpatel@gmail.com>
2016-06-02 18:48:30 +0530

    add unannprimitive type (error)

---
+++ group2JavaCompiler/group2JavaCompiler/AST/Unannprimitivetype.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace group2JavaCompiler.AST
+{
+    class Unannprimitivetype : Node
+    {
+    }
+
+    public class Unannprimitivetype : Type
+    {
+        public override void dump(int indent)
+        {
+            label(indent, "IntType\n");
+        }
+    }
+}
