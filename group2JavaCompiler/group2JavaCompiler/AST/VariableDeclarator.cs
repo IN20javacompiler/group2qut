dbf7aa5f6847d2fcd31f4709ce7974e9428a88a9
Brar <Brar>
2016-06-02 22:50:49 +1000

    VARIABLEDECLATORID DUMP

--- group2JavaCompiler/group2JavaCompiler/AST/VariableDeclarator.cs
+++ group2JavaCompiler/group2JavaCompiler/AST/VariableDeclarator.cs
@@ -11,22 +11,22 @@ namespace group2JavaCompiler.AST
         private VariableDeclaratorId id;
 
         private Expression init;
-        /*public VariableDeclarator(VariableDeclaratorId id)
+        public VariableDeclarator(VariableDeclaratorId id)
         {
             this.id=id;
-        }*/
+        }
 
             public VariableDeclarator(VariableDeclaratorId id, Expression init)
         {
             this.id = id;
             this.init = init;
         }
-        public override void dump(int indent)
+       /* public override void dump(int indent)
         {
             label(indent, "Variable Declaration\n");
             init.dump(indent + 1);
             id.dump(indent + 1);
             
-        }
+        }*/
     }
 }
