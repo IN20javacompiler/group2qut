eb2132d321571683b345aab3f8915bf372b3a556
Harsh Sanghavi <Harsh Sanghavi>
2016-06-03 17:21:29 +1000

    Programm.cs file added

--- TestJavaCompiler/TestJavaCompiler/Program.cs
+++ TestJavaCompiler/TestJavaCompiler/Program.cs
@@ -10,6 +10,15 @@ namespace TestJavaCompiler
     {
         static void Main(string[] args)
         {
+            var filename = args[0];
+            var file = new FileStream(args[0], FileMode.Open);
+            Scanner scanner = new Scanner(file);
+            Parser parser = new Parser(scanner);
+            parser.Parse();
+            Parser.root.dump(0);
+            var writer = new StreamWriter(filename + ".il");
+            writer.WriteLine("﻿.assembly {0} {{}}", filename);
+            writer.Close();
         }
     }
 }
